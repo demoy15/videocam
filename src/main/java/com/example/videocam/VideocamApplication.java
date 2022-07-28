1c8651634ce44fea796fdf4feddc55c8b9efb955
demoy <demoy16.alex@mail.ru>
2022-07-28 21:04:09 +0300

    Init application

---
+++ src/main/java/com/example/videocam/VideocamApplication.java
@@ -0,0 +1,13 @@
+package com.example.videocam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideocamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideocamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videocam/VideocamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videocam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
